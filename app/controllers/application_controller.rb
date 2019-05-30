c8654adbdbae1088771b1e0d0c110b778968092e
Rabbig <fi8608@naver.com>
2019-05-30 12:31:23 +0000

    avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 	protected 
 	def 
 		configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :website, :phone]) 
-		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :website, :phone]) 
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :website, :phone, :avatar]) 
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :website, :phone, :avatar]) 
 	end
 
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,11 +23,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+	process :resize_to_fit => [512,512]
   #
   # def scale(width, height)
   #   # do something
   # end
-
+  version :thumb do
+	  process :resize_to_fit => [32, 32]
+  end
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
