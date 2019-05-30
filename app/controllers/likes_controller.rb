23b1797ab265a16e15d3b04dca5321c63a10042f
Rabbig <fi8608@naver.com>
2019-05-30 13:29:15 +0000

    user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,8 @@ class LikesController < ApplicationController
 	def like_toggle
 		like = Like.find_by(user_id: current_user.id,
 							post_id: params[:post_id])
+		respond_to do |format|
+  			format.js {render inline: "location.reload();" }
 		if like.nil?
 			Like.create(user_id: current_user.id, 
 						post_id: params[:post_id])
@@ -11,6 +13,6 @@ class LikesController < ApplicationController
 			like.destroy
 		end
 		
-		redirect_to root_path
+	end
 	end
 end
